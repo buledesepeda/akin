e56cbe83e6fa25099af40d2d4b49a9c6ba37be08
blair <blairdesepeda@gmail.com>
2024-10-14 22:50:57 +0800

    asas

---
+++ dbconn.php
@@ -0,0 +1,17 @@
+<?php
+define ('hostname','localhost');
+define ('username','root');
+define ('password','');
+define ('database','curd');
+
+$conn=mysqli_connect(hostname,username,password,database);
+
+if(!$conn)
+{
+    die("Connection failed".mysqli_error());
+}
+else
+{
+    echo "Connected";
+}
+?>
